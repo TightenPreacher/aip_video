3dac16eee5a1dc8b47817bb7d9a9f82c8527c548
面包 <731995484@qq.com>
2018-03-13 15:35:52 +0800

    转化格式wav

---
+++ src/test.js
@@ -0,0 +1,10 @@
+var ffmpeg = require('fluent-ffmpeg');
+
+var command = ffmpeg('../asset/5-2 随堂小测试.mp4')
+			.on('error',function (err) {
+				console.log('错误: ' + err.message);
+			})
+			.on('end', function() {
+				console.log('转化成功!');
+			})
+			.save('../dist/5-2 随堂小测试.wav');
