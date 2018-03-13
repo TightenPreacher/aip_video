b974eb1cbd4a2cde728749161db16a7a76c7e4a8
面包 <731995484@qq.com>
2018-03-13 15:48:08 +0800

    转化格式wav

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 var ffmpeg = require('fluent-ffmpeg');
 
-var command = ffmpeg('../asset/5-2 随堂小测试.mp4')
+var command = ffmpeg('../asset/test.mp4')
 			.videoCodec('libx264')
 			.audioCodec('libmp3lame')
 			.on('error',function (err) {
@@ -9,4 +9,4 @@ var command = ffmpeg('../asset/5-2 随堂小测试.mp4')
 			.on('end', function() {
 				console.log('转化成功!');
 			})
-			.save('../dist/5-2 随堂小测试.wav');
+			.save('../dist/test.wav');
