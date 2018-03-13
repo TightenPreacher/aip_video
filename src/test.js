d70107b6c52a8ce51cb40d3409c84c44d4df99a9
面包 <731995484@qq.com>
2018-03-13 18:46:30 +0800

    转化格式wav

--- src/test.js
+++ src/test.js
@@ -1,21 +1,14 @@
 var ffmpeg = require('fluent-ffmpeg');
 
-// var command = ffmpeg('../asset/abc.flv')
-// 			.videoCodec('libx264')
-// 			.audioCodec('libmp3lame')
-// 			.size('320x240')
-// 			// .audioBitrate('16k')
-// 			.on('error',function (err) {
-// 				console.log('错误: ' + err.message);
-// 			})
-// 			.on('end', function() {
-// 				console.log('转化成功!');
-// 			})
-// 			.save('../dist/test.mp3');
-
-
 var command = ffmpeg('../asset/abc.flv')
 	.videoCodec('libx264')
-	.format('flv');
-
-command.save('../dist/abc.mp4');
+	.audioCodec('libmp3lame')
+	.size('320x240')
+	// .audioBitrate('16k')
+	.on('error',function (err) {
+		console.log('错误: ' + err.message);
+	})
+	.on('end', function() {
+		console.log('转化成功!');
+	})
+	.save('../dist/test.mp3');
