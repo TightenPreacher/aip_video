0aa9329f35096b5500f7de75924f5a8282a54e52
TightenPreacher <731995484@qq.com>
2018-03-13 23:36:23 +0800

    1.toMp4

--- src/test.js
+++ src/test.js
@@ -1,6 +1,7 @@
 var ffmpeg = require('fluent-ffmpeg');
 
-var command = ffmpeg('../asset/abc.flv')
+//针对.flv格式
+//If you intend to encode FLV videos, you must have either flvtool2 or flvmeta installed and in your PATH or fluent-ffmpeg won't be able to produce streamable output files. If you set either the FLVTOOL2_PATH or FLVMETA_PATH, fluent-ffmpeg will try to use it instead of searching in the PATH.var command = ffmpeg('../asset/abc.flv')
 	.videoCodec('libx264')
 	.format('mp4')
 	// .audioBitrate('16k')
