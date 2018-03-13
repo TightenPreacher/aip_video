97e6b6aee2696030372a8253cdf9e2675adafd7e
TightenPreacher <731995484@qq.com>
2018-03-13 23:02:57 +0800

    1.toMp4

--- src/test.js
+++ src/test.js
@@ -1,9 +1,9 @@
 var ffmpeg = require('fluent-ffmpeg');
 
 var command = ffmpeg('../asset/abc.flv')
+	.audioCodec('libfaac')
 	.videoCodec('libx264')
-	.audioCodec('libmp3lame')
-	.size('320x240')
+	.format('mp4');
 	// .audioBitrate('16k')
 	.on('error',function (err) {
 		console.log('错误: ' + err.message);
@@ -11,4 +11,4 @@ var command = ffmpeg('../asset/abc.flv')
 	.on('end', function() {
 		console.log('转化成功!');
 	})
-	.save('../dist/test.mp3');
+	.save('../dist/test.mp4');
