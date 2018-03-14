840ce5206b399f96f072551f0fe771b95e4f6038
TightenPreacher <731995484@qq.com>
2018-03-14 22:37:13 +0800

    dd

--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ var ffmpeg = require('fluent-ffmpeg');
 
 //针对.flv格式
 //If you intend to encode FLV videos, you must have either flvtool2 or flvmeta installed and in your PATH or fluent-ffmpeg won't be able to produce streamable output files. If you set either the FLVTOOL2_PATH or FLVMETA_PATH, fluent-ffmpeg will try to use it instead of searching in the PATH.var command = ffmpeg('../asset/abc.flv')
+	var command = ffmpeg('../asset/abc')
 	.videoCodec('libx264')
 	.format('mp4')
 	// .audioBitrate('16k')
@@ -11,4 +12,4 @@ var ffmpeg = require('fluent-ffmpeg');
 	.on('end', function() {
 		console.log('转化成功!');
 	})
-	.save('../dist/test.mp4');
+	.save('../dist/abc.mp3');
